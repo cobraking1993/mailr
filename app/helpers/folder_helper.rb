57a1acb98ec1116e5b7d4c000ccb4da84e11b3cf
Wojciech Todryk <wojciech@todryk.pl>
2013-03-15 20:19:45 +0100

    folders icon configuration and refactories of folder helper

--- app/helpers/folder_helper.rb
+++ app/helpers/folder_helper.rb
@@ -1,72 +1,66 @@
 module FolderHelper
 
-    def folder_link(options={})
-			
-				folder = options[:folder]
-				active = ""
-				if options[:active]
-					active = "icon-white"
-				end
+  def pretty_folder_name(folder)
+    if folder.nil?
+      t(:no_selected, :scope=>:folder)
+    else
+      if folder.isInbox?
+          t(:inbox_name,:scope => :folder)
+      elsif folder.isSent?
+          t(:sent_name,:scope => :folder)
+      elsif folder.isDrafts?
+          t(:drafts_name,:scope => :folder)
+      elsif folder.isTrash?
+          t(:trash_name,:scope => :folder)
+      else
+          folder.name.capitalize
+      end
+    end
+  end
 
-        folder.parent.empty? ? name = folder.name : name = folder.parent.gsub(/\./,'#') + "#" + folder.name
+  # Folder link consists of three elements: icon, name, info. Info can be a label showing unreaded messages or link to empty trashbin.
+  # 
+  def folder_link(options={})
+		
+		folder = options[:folder]
+    active = options[:active]
+    sys_icons = $defaults["system_folders_icons"]
+    other_icon = $defaults["other_folders_icon"]
 
-        if folder.isInbox?
-            name_shown = "<i class=\"icon-inbox #{active}\"></i>" + t(:inbox_name,:scope => :folder)
-        elsif folder.isSent?
-            name_shown = "<i class=\"icon-plane #{active}\"></i>" + t(:sent_name,:scope => :folder)
-        elsif folder.isDrafts?
-            name_shown = "<i class=\"icon-book #{active}\"></i>" + t(:drafts_name,:scope => :folder)
-        elsif folder.isTrash?
-            name_shown = "<i class=\"icon-trash #{active}\"></i>" +t(:trash_name,:scope => :folder)
-        else
-            name_shown = "<i class=\"icon-none\"></i>" + folder.name.capitalize
-        end
-        
-        if folder.isTrash?
-            if not folder.total.zero?
-                name_shown += " <button class=\"btn btn-mini btn-danger\" onclick=\"window.location='#{folders_emptybin_path}'\" href=\"#\">#{t(:emptybin,:scope=>:folder)}</button>"
-                #name_shown += raw link_to(t(:emptybin,:scope=>:folder),folders_emptybin_path)
-                #name_shown += ')'
-            end
-        else
-            if !folder.unseen.zero?
-                name_shown += " <span class=\"label label-important\">#{folder.unseen.to_s}</span>"
-            end
-        end
-        
-        link_to name_shown.html_safe, folders_select_path(:id => name)
+    folder_link = folder.parent.empty? ? folder.name : folder.parent.gsub(/\./,'#') + "#" + folder.name
+    icon = folder.sys > 0 ? sys_icons[folder.sys - 1] : other_icon
+    icon_tag = content_tag(:i, "", :class => (c = "icon-#{icon}"; c += " icon-white" if active and folder.sys; c))
+    info = ""
+    
+    name_shown = pretty_folder_name(folder)
+
+    if folder.isTrash?
+      info = content_tag(:button,
+                           t(:emptybin, :scope=>:folder),
+                           :type => "submit",
+                           :class => "btn btn-mini btn-danger folder_action") unless folder.total.zero?
+      info = content_tag(:form, info, :action => folders_emptybin_path)
     end
 
-    def pretty_folder_name(folder)
-        if folder.nil?
-            t(:no_selected,:scope=>:folder)
-        else
-            if folder.isInbox?
-                t(:inbox_name,:scope => :folder)
-            elsif folder.isSent?
-                t(:sent_name,:scope => :folder)
-            elsif folder.isDrafts?
-                t(:drafts_name,:scope => :folder)
-            elsif folder.isTrash?
-                t(:trash_name,:scope => :folder)
-            else
-                folder.name.capitalize
-            end
-        end
+    unless (folder.isTrash? or folder.unseen.zero?)
+      info = content_tag(:span, folder.unseen.to_s, :class => "label label-important folder_info")
     end
+    
+    content_tag(:a,icon_tag + name_shown, :href => folders_select_path(:id => folder_link)) + info
+  end
 
-    def select_for_folders(name,id,collection,label,choice,blank)
-        html = ""
-        html << "<div class=\"param_group\">"
-        html << "<label class=\"label\">#{label}</label>"
-        html << simple_select_for_folders(name,id,collection,choice,blank)
-        html << "</div>"
-    end
+  def select_for_folders(name,id,collection,label,choice,blank)
+    html = ""
+    html << "<div class=\"param_group\">"
+    html << "<label class=\"label\">#{label}</label>"
+    html << simple_select_for_folders(name,id,collection,choice,blank)
+    html << "</div>"
+  end
 
-    def simple_select_for_folders(name,id,collection,choice,blank)
-        html = ""
-        html << select(name , id, options_from_collection_for_select(collection, 'id', 'full_name', choice),{ :include_blank => (blank == true ? true : false)})
-        html
-    end
+  def simple_select_for_folders(name,id,collection,choice,blank)
+    html = ""
+    html << select(name , id, options_from_collection_for_select(collection, 'id', 'full_name', choice),{ :include_blank => (blank == true ? true : false)})
+    html
+  end
 
 end
---
+++ spec/helpers/folder_helper_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+require 'folder_helper'
+
+describe "folder_link" do
+
+  before :all do
+    $defaults ||= YAML::load_file(Rails.root.join('config','settings.yml'))
+  end
+
+  let(:folder){Folder.new(:parent => "",
+                          :name => "messages",
+                          :delim => ".",
+                          :haschildren => false,
+                          :total => 0, 
+                          :unseen => 0)}
+
+  it "trash folder has messages" do
+    folder.sys = Folder::SYS_TRASH
+    folder.total = 2
+    helper.folder_link(:folder => folder, :active => true).should eq("<a href=\"/folders/select/messages\"><i class=\"icon-trash icon-white\"></i>Trash</a><form action=\"/folders/emptybin\"><button class=\"btn btn-mini btn-danger folder_action\" type=\"submit\">Empty bin</button></form>")
+  end
+
+  it "system folder unseen messages active" do
+    folder.sys = Folder::SYS_INBOX
+    folder.total = 2
+    folder.unseen = 2
+    helper.folder_link(:folder => folder, :active => true).should eq("<a href=\"/folders/select/messages\"><i class=\"icon-inbox icon-white\"></i>Inbox</a><span class=\"label label-important folder_info\">2</span>")
+  end
+
+  it "system folder unseen messages not active" do
+    folder.sys = Folder::SYS_INBOX
+    folder.total = 2
+    folder.unseen = 2
+    helper.folder_link(:folder => folder, :active => false).should eq("<a href=\"/folders/select/messages\"><i class=\"icon-inbox\"></i>Inbox</a><span class=\"label label-important folder_info\">2</span>")
+  end
+
+  it "system folder no unseen messages not active" do
+    folder.sys = Folder::SYS_INBOX
+    folder.unseen = 0
+    folder.total = 0
+    helper.folder_link(:folder => folder, :active => false).should eq("<a href=\"/folders/select/messages\"><i class=\"icon-inbox\"></i>Inbox</a>")
+  end
+
+end

4728710348cd6cddd8f0eabd83b57e4b34a0ebbb
Wojciech Todryk <wojciech@todryk.pl>
2013-11-25 21:43:32 +0100

    responsive initial

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mailr::Application.config.secret_token = '014b3111b40bea36f7c0ab07ff2ae820f71b19bb67970baf1f7b44dc1555aae92a68395db757548058884dd4884b1267823d4d44a783160a595e2668d2e904a8'
